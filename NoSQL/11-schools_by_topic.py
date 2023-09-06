fbd0a761abb5c6bc5634eeb07ff771a896a44b97
20Emi <iop.emilysanchez1@gmail.com>
2023-09-06 07:46:16 -0700

    task 11

--- NoSQL/11-schools_by_topic.py
+++ NoSQL/11-schools_by_topic.py
@@ -7,7 +7,7 @@ import pymongo
 def schools_by_topic(mongo_collection, topic):
     """function that returns the list of school having a speciific topic"""
 
-    collection = mongo_collection.find({'topic': topic})
+    collection = mongo_collection.find({'topics': topic})
     list = []
     for fi in collection:
         list.append(fi)
