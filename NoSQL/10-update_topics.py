070837060be9ac94b7f97d044b89227aaa807d2c
20Emi <iop.emilysanchez1@gmail.com>
2023-09-06 07:35:24 -0700

    task 10

---
+++ NoSQL/10-update_topics.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Task 10"""
+
+import pymongo
+
+
+def update_topics(mongo_collection, name, topics):
+    """function that changes all topics of a school document based on the name"""
+
+    filter_ = {'name': name}
+    new_data = {'$set': {'topics': topics}}
+    result = mongo_collection.update_many(filter_, new_data)
+    return result
