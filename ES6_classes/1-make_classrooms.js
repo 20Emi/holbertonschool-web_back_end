4b1e2c3d0b92d29e89a206717fe7ce96502fcb87
20Emi <iop.emilysanchez1@gmail.com>
2023-09-15 06:52:04 -0700

    task 1

--- ES6_classes/1-make_classrooms.js
+++ ES6_classes/1-make_classrooms.js
@@ -1,10 +1,9 @@
 import ClassRoom from './0-classroom';
 
 export default function initializeRooms() {
-
   const instanceA = new ClassRoom(19);
   const instanceB = new ClassRoom(20);
   const instanceC = new ClassRoom(34);
-  const obj = [instanceA, instanceB, instanceC]
-  return obj
+  const obj = [instanceA, instanceB, instanceC];
+  return obj;
 }
