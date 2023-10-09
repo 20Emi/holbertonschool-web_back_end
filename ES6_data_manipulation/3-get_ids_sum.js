07958bc54e9d024c9887dced42a02b2f923a82db
20Emi <iop.emilysanchez1@gmail.com>
2023-10-09 06:52:23 -0700

    task 3, Passes lint

--- ES6_data_manipulation/3-get_ids_sum.js
+++ ES6_data_manipulation/3-get_ids_sum.js
@@ -1,4 +1,4 @@
 export default function getStudentIdsSum(student) {
-    let SumaIds = student.reduce((acumulador, element) => acumulador + element['id'], 0) 
-    return SumaIds
+  const SumaIds = student.reduce((acumulador, element) => acumulador + element.id, 0);
+  return SumaIds;
 }
