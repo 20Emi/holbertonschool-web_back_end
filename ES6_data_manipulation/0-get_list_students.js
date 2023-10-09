5976df17efdadee0c075e8e08872ebee5003e3d4
20Emi <iop.emilysanchez1@gmail.com>
2023-10-09 05:17:36 -0700

    task 0

--- ES6_data_manipulation/0-get_list_students.js
+++ ES6_data_manipulation/0-get_list_students.js
@@ -1,9 +1,9 @@
 export default function getListStudents() {
   const arrayofobjects = [];
 
-  arrayofobjects.push({id: '1', firstName: 'Guillaume', location: 'San Francisco'});
-  arrayofobjects.push({id: '2', firstName: 'James', location: 'Columbia'});
-  arrayofobjects.push({id: '5', firstName: 'Serena', location: 'San Francisco'});
+  arrayofobjects.push({ id: 1, firstName: 'Guillaume', location: 'San Francisco' });
+  arrayofobjects.push({ id: 2, firstName: 'James', location: 'Columbia' });
+  arrayofobjects.push({ id: 5, firstName: 'Serena', location: 'San Francisco' });
 
   return arrayofobjects;
 }
